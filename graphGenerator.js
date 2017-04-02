aa35fb57629a50b9661824f988586886169e647c
Ouadie <ouadie@outlook.com>
2017-04-02 15:47:51 +0200

    Implementing the generator

--- graphGenerator.js
+++ graphGenerator.js
@@ -1,3 +1,21 @@
-module.exports = class Node {
-  constructor() {}
+const Node =  class {
+  constructor(name, value) {
+    this.name = name;
+    this.value = value;
+    this.neighbors = [];
+  }
+
+  addNeighbors(neighbors){
+    if(neighbors){
+      this.neighbors = this.neighbors.concat(neighbors);
+    }
+
+    return this.neighbors;
+  }
+
+  getNeighbors(){
+    return this.neighbors;
+  }
 }
+
+module.exports = Node;
